57ff184b1061ae2bdf01ca94ed3663f7a1c86de8
gdave001 <gabe.davenport97@gmail.com>
2025-10-06 18:50:43 -0400

    [FEAT] Enemy Ship Spawner
    Created an enemy prefab to periodically spawn in new alien ships.

---
+++ Assets/Scripts/AlienSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class AlienSpawner : MonoBehaviour
+{
+    [Header("Spawning")]
+    public GameObject alienPrefab;
+    public float spawnRate = 2f;
+    public Transform[] spawnPoints;
+
+    private float nextSpawnTime = 0f;
+
+    void Update()
+    {
+        if (Time.time >= nextSpawnTime)
+        {
+            spawnEnemy();
+            nextSpawnTime = Time.time + spawnRate;
+        }
+        if (GameManager.Instance.score > 400 && GameManager.Instance.score < 900)
+            spawnRate = 1.5f;
+        if (GameManager.Instance.score > 900 && GameManager.Instance.score < 1400)
+            spawnRate = 1.0f;
+        if (GameManager.Instance.score > 1400 && GameManager.Instance.score < 2000)
+            spawnRate = 0.5f;
+    }
+    private void spawnEnemy()
+    {
+        if (alienPrefab && spawnPoints.Length > 0)
+        {
+            if (GameManager.Instance.lives > 0)
+            {
+                int randomIndex = Random.Range(0, spawnPoints.Length);
+                Instantiate(alienPrefab, spawnPoints[randomIndex].position, Quaternion.identity);
+            }
+        }
+        
+    }
+}
