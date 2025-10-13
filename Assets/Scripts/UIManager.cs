cd2ac3f8f5908ae469adeebc3ca85ac74ecf174f
gdave001 <gabe.davenport97@gmail.com>
2025-10-13 17:34:05 -0400

    [FEAT] Created UIManager Observer
    Added an observer script to handle all UI update conditions (i.e. current scene, score, health, enemies killed and powerups collected).

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public TMP_Text scoreTxt;
+    public TMP_Text text;
+
+    private void Start()
+    {
+        Debug.Log("UI Manager active");
+
+        EventManager.Subscribe("OnScoreChanged", UpdateScore);
+        EventManager.Subscribe("OnPlayerHealthChanged", UpdateHealth);
+        EventManager.Subscribe("OnEnemyDefeated", UpdateTBD);
+        EventManager.Subscribe("OnLevelComplete", UpdateScene);
+        EventManager.Subscribe("OnPowerUpCollected", UpdateTBD);
+    }
+
+    private void UpdateScene()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateTBD()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateHealth()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateScore()
+    {
+        throw new NotImplementedException();
+    }
+}
